05731f85ef4cb811d68492149e4b73e6d407ec7c
kelly galliani <kelly_92b@hotmail.com>
2022-12-22 17:22:05 -0300

    fix: search

--- src/components/input/InputSearch/index.tsx
+++ src/components/input/InputSearch/index.tsx
@@ -4,11 +4,14 @@ import { useContext } from 'react';
 import { UserContext } from '../../../contexts/UserContext';
 
 export const InputSearch = () => {
-  const { userLogout, products,setProducts, filteredProducts, setFilteredProducts } = useContext(UserContext)
+  const { products,setProducts } = useContext(UserContext)
   
   function filter(event:React.ChangeEvent<HTMLInputElement>){
     const filteredList = products.filter((product) => (product.name.toLowerCase().includes(event.currentTarget.value)))
     setProducts(filteredList)
+    if(event.currentTarget.value){
+      setProducts(products)
+    }
   } 
   return (
     <StyledInputSearchBox>
